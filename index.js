4fb09f2af05c6a284896c4e139622f66d765b9c0
viniciustlima <vinitlima2007.14@gmail.com>
2021-11-19 17:28:12 -0300

    refactor

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-const { Client, Intents } = require('discord.js')
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] })
-const config = require('./config.json')
+const Discord = require("discord.js");
+const client = new Discord.Client({ intents: [Discord.Intents.FLAGS.GUILDS] });
 
-const command = require('./command')
+const config = require("./config.json");
+const command = require("./command");
 
-client.on('ready', () => {
-	console.log(`Client is ready! @${client.user.username}`)
-})
+client.on("ready", () => {
+  console.log(`Client is ready! @${client.user.username}`);
 
-command(client, 'ping', (message) => {
-	console.log(message)
-})
+  command(client, "ping", (message) => {
+    message.channel.send("Pong");
+  });
+});
 
-client.login(config.token)
+client.login(config.token);
