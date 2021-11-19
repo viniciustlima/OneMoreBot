073e07296d1263828033dced398bf1843c1214fe
viniciustlima <vinitlima2007.14@gmail.com>
2021-11-19 17:28:07 -0300

    refactor

--- command.js
+++ command.js
@@ -1,19 +1,21 @@
-const config = require('./config.json')
+const { prefix } = require("./config.json");
 
 module.exports = (client, aliases, callback) => {
-	if (typeof aliases === 'string') aliases = [aliases]
+  if (typeof aliases === "string") {
+    aliases = [aliases];
+  }
 
-	client.on('message', (message) => {
-		const { content } = message
-		const { author } = message
+  client.on("message", (message) => {
+    const { content } = message;
+    const { author } = message;
 
-		aliases.forEach((alias) => {
-			const command = `${config.prefix}${alias}`
+    aliases.forEach((alias) => {
+      const command = `${prefix}${alias}`;
 
-			if (content.startsWith(`${command} `) || content == command) {
-				console.log(`Command ${command} is being executed by ${author.tag}`)
-				callback()
-			}
-		})
-	})
-}
+      if (content.startsWith(`${command} `) || content === command) {
+        console.log(`Command ${command} is being executed by ${author.tag}`);
+        callback(message);
+      }
+    });
+  });
+};
