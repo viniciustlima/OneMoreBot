982e75ddeed01c461bcc74f586c19d5965b153f1
viniciustlima <vinitlima2007.14@gmail.com>
2021-11-19 17:08:28 -0300

    command function

---
+++ command.js
@@ -0,0 +1,19 @@
+const config = require('./config.json')
+
+module.exports = (client, aliases, callback) => {
+	if (typeof aliases === 'string') aliases = [aliases]
+
+	client.on('message', (message) => {
+		const { content } = message
+		const { author } = message
+
+		aliases.forEach((alias) => {
+			const command = `${config.prefix}${alias}`
+
+			if (content.startsWith(`${command} `) || content == command) {
+				console.log(`Command ${command} is being executed by ${author.tag}`)
+				callback()
+			}
+		})
+	})
+}
